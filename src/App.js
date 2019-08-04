e9a7fd2720e4f63ac1606d5df3009dde755395b0
Rafa Mel <melrcm@gmx.net>
2019-08-04 02:14:03 +0200

    adds framework7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
+import { App as Application } from 'framework7-react';
 import logo from './logo.svg';
 import './App.css';
 
+export const params = {
+  id: 'io.app.myapp',
+  name: 'My App',
+  // Automatic theme detection
+  theme: 'auto'
+};
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Application params={params}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Application>
   );
 }
 
